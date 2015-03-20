6d94676d05186f171e64cd24da6a583cafdeb6c1
GustavoCaso <gustavocaso@gmail.com>
2015-03-20 15:17:28 +0100

    Update factory_generator.rb

--- lib/factory_generator.rb
+++ lib/factory_generator.rb
@@ -2,38 +2,53 @@ require "factory_generator/version"
 
 module FactoryGenerator
   class FactoryGenerator
-    def initialize(object)
-      @object = object.respond_to?('first') ? object.first : object
-      @attributes = @object.respond_to?('first') ? @object.first.attributes : @object.attributes
-      return if File.file?("#{@object.class.name.downcase}.rb")
-      create_file("#{@object.class.name.downcase}.rb")
-      @key_columns = AssociationManager.new(@object).get_association_info
+    def initialize(object, options = {})
+      options = {
+          nested: false,
+          overwrite: false,
+          parent: [],
+          skip_params: [],
+          params: {}
+      }.merge(options)
+  
+      @object = get_object(object)
+      @parent = options[:parent]
+      klass = @object.class.name.downcase
+      @attributes = @object.attributes
+      options[:skip_params].each { |x|  @attributes.delete(x)} unless options[:skip_params].empty?
+      @attributes.merge!(options[:params]) unless options[:params].empty?
+      @association = AssociationManager.new(@object)
+      @file = FileManager.new(klass)
+      @file.create_file
+      @key_columns = @association.get_association_info
       @factory_body = ''
-      write_associations
+      write_associations if options[:nested]
       write_attributes
-      text = write_factory(@object, @factory_body)
-      write_file(text, @object.class.name.downcase)
+      text = write_factory(klass, @factory_body)
+      @file.write_file(text)
     end
 
-    def write_factory(object, attributes)
+    def write_factory(class_name, attributes)
       <<-EOF
-FactoryGirl.define do
-  factory :#{object.class.name.downcase} do
-    #{attributes}
+  FactoryGirl.define do
+    factory :#{class_name.downcase} do
+  #{attributes}
+    end
   end
-end
       EOF
     end
 
     def write_associations
       @key_columns.keys.each do |method|
         begin
-          puts "#{method}"
           object =  @object.send(method)
+          object = get_object(object)
+          object_class = object.class.name
           next if object.nil? || (object.respond_to?('first') && object.empty?)
-          self.class.new(object)
-          binding.pry if AssociationManager.new(@object).determine_association(@key_columns[method]).nil?
-          @factory_body += AssociationManager.new(@object).determine_association(@key_columns[method])
+          parent_included = @parent.include? object_class
+          (@parent << object_class).uniq!
+          self.class.new(object, nested: true, parent: @parent) unless parent_included
+          @factory_body += @association.determine_association(@key_columns[method], object_class, method)
           @attributes.delete_if {|k,v| k =~ Regexp.new(method) && !v.nil?}
         rescue => e
           puts "There was an error creating the association #{e} => #{e.backtrace}"
@@ -44,31 +59,15 @@ end
 
     def write_attributes
       @attributes.each do |k,v|
-        @factory_body += "#{k} #{map_values(v)}\n"
+        value =  ValueMapper.new(@object, k.dup, v).map_values
+        key = ValueMapper.remove_encrypted(k.dup)
+        @factory_body += "    #{key} #{value}\n"
       end
     end
-
-    def map_values(value)
-      if value.class == String || value.class == ActiveSupport::TimeWithZone
-        return_val = "\'#{value}\'"
-      elsif value.nil?
-        return_val = "nil"
-      else
-        return_val = value
-      end
-      return_val
-    end
-
-    def write_file(text, title)
-      puts "writing file #{title}"
-      File.open("#{title}.rb", 'w') do |f|
-        f.write text
-      end
-    end
-
-    def create_file(title)
-      puts "creating file #{title}"
-      File.new(title, "w")
+  
+    def get_object(object)
+      object.respond_to?('first') ? object.first : object
     end
   end
+
 end
