1c313d5caee1cdcb3344c32b2c22b0139d27cd96
GustavoCaso <gustavocaso@gmail.com>
2015-03-22 16:26:45 +0100

    Working on a new wey of implementation

--- lib/factory_generator/file_manager.rb
+++ lib/factory_generator/file_manager.rb
@@ -1,25 +1,44 @@
 module FactoryGenerator
+
+    attr_accessor :path
+
     class FileManager
-      def initialize(name)
+      def initialize(name, overwrite)
         @name = name
-        @title = "#{@name}.rb"
-        @file_path = File.join(FactoryGenerator.configuration.factory_directory, @title)
-    end
+        @overwrite = overwrite
+      end
 
     def create_file
-      puts "creating file #{@title}"
-      File.new(@file_path, "w")
+      puts "creating file #{@path}"
+      File.new(@path, "w")
     end
 
     def check_existence?
-      File.file?(@file_path)
+      File.file?(@path)
     end
 
     def write_file(text)
-      puts "writing file #{@title}"
-      File.open(@file_path, 'w') do |f|
+      puts "writing file #{@path}"
+      File.open(@path, 'w') do |f|
         f.write text
       end
+    end
+
+    def check_and_create_file
+      create_path
+      if check_existence? && @overwrite
+        create_file
+      elsif check_existence? && !@overwrite
+        create_path("#{@name}_2")
+        create_file
+      else
+        create_file
+      end
+      self
+    end
+
+    def create_path(name = @name)
+      @path = File.join(FactoryGenerator.configuration.factory_directory, "#{name}.rb")
     end
   end
 end
--- lib/factory_generator/generator.rb
+++ lib/factory_generator/generator.rb
@@ -10,47 +10,56 @@ module FactoryGenerator
     }
 
     def initialize(object, options = {})
-      options = DEFAULT_OPTIONS.merge(options)
-
-      @object = get_object(object)
-      @parent = options[:parent]
-      klass = @object.class.name.downcase
-      @attributes = @object.attributes
-      options[:skip_params].each { |x|  @attributes.delete(x)} unless options[:skip_params].empty?
-      @attributes.merge!(options[:params]) unless options[:params].empty?
-      @association = AssociationManager.new(@object)
-      @file = FileManager.new(klass)
-      @file.create_file
-      @key_columns = @association.get_association_info
-      @factory_body = ''
-      write_associations if options[:nested]
-      write_attributes
-      text = write_factory(klass, @factory_body)
-      @file.write_file(text)
+      @options = DEFAULT_OPTIONS.merge(options)
+      object = get_object(object)
+      @existing_factories = FactoryGenerator.configuration.factory_names
+      generate(object)
     end
 
+    def generate(object)
+      attributes =  manipulate_attributes(object.attributes)
+      association_body = ''
+      association_body, attributes = write_associations(object, attributes, association_body) if @options[:nested]
+      klass = object.class.name.downcase
+      association_body = write_attributes(object, attributes ,association_body)
+      file_manager =  generate_file(klass)
+      text = write_factory(klass, association_body)
+      file_manager.write_file(text)
+    end
+
+    def manipulate_attributes(attributes)
+      attr = attributes
+      @options[:skip_params].each { |x|  attr.delete(x)} unless @options[:skip_params].empty?
+      attr.merge!(@options[:params]) unless @options[:params].empty?
+      attr
+    end
+
+
     def write_factory(class_name, attributes)
+      name = @existing_factories.include?(class_name) ? "#{class_name}_2" : class_name
       <<-EOF
 FactoryGirl.define do
-  factory :#{class_name.downcase} do
+  factory :#{name}, class: #{class_name.capitalize.constantize} do
 #{attributes}
   end
 end
       EOF
     end
 
-    def write_associations
-      @key_columns.keys.each do |method|
+    def write_associations(object, attributes, association_body)
+      association = AssociationManager.new(object)
+      columns_assosiation_hash = association.get_association_info
+      columns_assosiation_hash.keys.each do |method|
         begin
-          object =  @object.send(method)
-          object = get_object(object)
+          object = get_object(object.send(method))
           object_class = object.class.name
           next if object.nil? || (object.respond_to?('first') && object.empty?)
-          parent_included = @parent.include? object_class
-          (@parent << object_class).uniq!
-          self.class.new(object, nested: true, parent: @parent) unless parent_included
-          @factory_body += @association.determine_association(@key_columns[method], object_class, method)
-          @attributes.delete_if {|k,v| k =~ Regexp.new(method) && !v.nil?}
+          parent_included = @options[:parent].include? object_class
+          (@options[:parent] << object_class).uniq!
+          generate(object) unless parent_included
+          association_body += association.determine_association(columns_assosiation_hash[method], object_class, method)
+          attributes.delete_if {|k,v| k =~ Regexp.new(method) && !v.nil?}
+          [association_body, attributes]
         rescue => e
           puts "There was an error creating the association #{e} => #{e.backtrace}"
           next
@@ -58,12 +67,18 @@ end
       end
     end
 
-    def write_attributes
-      @attributes.each do |k,v|
-        value =  ValueMapper.new(@object, k.dup, v).map_values
+    def write_attributes(object, attributes, association_body)
+      body = association_body
+      attributes.each do |k,v|
+        value =  ValueMapper.new(object, k.dup, v).map_values
         key = ValueMapper.remove_encrypted(k.dup)
-        @factory_body += "    #{key} #{value}\n"
+        body += "    #{key} #{value}\n"
       end
+      body
+    end
+
+    def generate_file(klass)
+      FileManager.new(klass, @options[:overwrite]).check_and_create_file
     end
 
     def get_object(object)
