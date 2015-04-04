4a682f557c6f02307b4576fbcd067474d1fd0330
GustavoCaso <gustavocaso@gmail.com>
2015-04-05 00:24:49 +0200

    Try to push gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'active_record'
 require 'factory_generator'
-require 'pry'
 
 ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
 load File.dirname(__FILE__) + '/support/schema.rb'
